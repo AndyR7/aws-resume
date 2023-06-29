caa65cf28f42ecbd8a7e96b6143f5c0a70e88015
Andy Ruvalcaba <andy.ruvalcaba7@gmail.com>
2023-06-28 23:59:21 -0700

    Updates to js/viewcounter.js

--- frontend/js/viewcounter.js
+++ frontend/js/viewcounter.js
@@ -1,4 +1,4 @@
-// Replace this URL with the URL of your Azure Function
+// Replace this URL with the URL of your  AWS Lambda function
 const url = 'https://mgewr4tcapsrfwqwfptlhhautq0bekzi.lambda-url.us-east-1.on.aws/';
 
 // If you have a visitor ID stored in a cookie, include it in the query parameters
@@ -6,9 +6,9 @@ const url = 'https://mgewr4tcapsrfwqwfptlhhautq0bekzi.lambda-url.us-east-1.on.aw
 // const urlWithVisitorId = url + '?visitorId=' + visitorId;
 
 fetch(url)
-    .then(response => response.text())
+    .then(response => response.json())
     .then(data => {
-      // Update the counter on the page with the response from the Azure Function
+      // Update the counter on the page with the response from the  AWS Lambda function
       document.getElementById('counter').textContent = data;
     })
     .catch(error => {
